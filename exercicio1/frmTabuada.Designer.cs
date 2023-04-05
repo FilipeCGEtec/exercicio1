8ab3afc803a41aeb7507dc5120c5a8c84e6b9037
DreiJD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-04-05 20:13:32 -0300

    Exercicio da Tabuada

---
+++ exercicio1/frmTabuada.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace exercicio1
+{
+    partial class frmTabuada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblPedido = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnCalcular3 = new System.Windows.Forms.Button();
+            this.btnCalcular2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(389, 155);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(281, 155);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(127, 155);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(121, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular (for)";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(80, 97);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(55, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Resultado";
+            // 
+            // lblPedido
+            // 
+            this.lblPedido.AutoSize = true;
+            this.lblPedido.Location = new System.Drawing.Point(80, 55);
+            this.lblPedido.Name = "lblPedido";
+            this.lblPedido.Size = new System.Drawing.Size(87, 13);
+            this.lblPedido.TabIndex = 4;
+            this.lblPedido.Text = "Insira um número";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(173, 94);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(291, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(173, 48);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 6;
+            // 
+            // btnCalcular3
+            // 
+            this.btnCalcular3.Location = new System.Drawing.Point(127, 213);
+            this.btnCalcular3.Name = "btnCalcular3";
+            this.btnCalcular3.Size = new System.Drawing.Size(121, 23);
+            this.btnCalcular3.TabIndex = 7;
+            this.btnCalcular3.Text = "Calcular (do)";
+            this.btnCalcular3.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular2
+            // 
+            this.btnCalcular2.Location = new System.Drawing.Point(127, 184);
+            this.btnCalcular2.Name = "btnCalcular2";
+            this.btnCalcular2.Size = new System.Drawing.Size(121, 23);
+            this.btnCalcular2.TabIndex = 8;
+            this.btnCalcular2.Text = "Calcular (while)";
+            this.btnCalcular2.UseVisualStyleBackColor = true;
+            // 
+            // frmTabuada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular2);
+            this.Controls.Add(this.btnCalcular3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblPedido);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVoltar);
+            this.Name = "frmTabuada";
+            this.Text = "frmTabuada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblPedido;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnCalcular3;
+        private System.Windows.Forms.Button btnCalcular2;
+    }
+}
---
+++ exercicio1/frmTabuada.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio1
+{
+    public partial class frmTabuada : Form
+    {
+        public frmTabuada()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double x, result;
+            int i;
+
+            x = Convert.ToDouble(txtNum.Text);
+            for (i = 1; i < 10; i++) {
+                result = x * i;
+
+                txtResult.Text = String.Concat(txtResult.Text, " \n\n ", result.ToString()); ;
+            }
+        }
+    }
+}
