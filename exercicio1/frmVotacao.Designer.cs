2d21ada5874867eee5371e64d6f8b1dc1e5bf3ef
DreiJD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-04-05 21:23:17 -0300

    Exercicio Votação

--- exercicio1/frmVotacao.Designer.cs
+++ exercicio1/frmVotacao.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtVoto = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.lblVoto = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnFim = new System.Windows.Forms.Button();
+            this.btnVotar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtVoto
+            // 
+            this.txtVoto.Location = new System.Drawing.Point(238, 90);
+            this.txtVoto.Name = "txtVoto";
+            this.txtVoto.Size = new System.Drawing.Size(211, 20);
+            this.txtVoto.TabIndex = 0;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(140, 137);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(309, 20);
+            this.txtFinal.TabIndex = 1;
+            // 
+            // lblVoto
+            // 
+            this.lblVoto.AutoSize = true;
+            this.lblVoto.Location = new System.Drawing.Point(137, 93);
+            this.lblVoto.Name = "lblVoto";
+            this.lblVoto.Size = new System.Drawing.Size(77, 13);
+            this.lblVoto.TabIndex = 3;
+            this.lblVoto.Text = "Insira seu Voto";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(137, 49);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(312, 13);
+            this.lblTitulo.TabIndex = 4;
+            this.lblTitulo.Text = "1- Jossé da Silva (js), 2- Maria Juruma(mj), 3- João da Tapioca (jt)";
+            // 
+            // btnFim
+            // 
+            this.btnFim.Location = new System.Drawing.Point(174, 221);
+            this.btnFim.Name = "btnFim";
+            this.btnFim.Size = new System.Drawing.Size(275, 23);
+            this.btnFim.TabIndex = 5;
+            this.btnFim.Text = "Finalizar Votação";
+            this.btnFim.UseVisualStyleBackColor = true;
+            this.btnFim.Click += new System.EventHandler(this.btnFim_Click);
+            // 
+            // btnVotar
+            // 
+            this.btnVotar.Location = new System.Drawing.Point(174, 180);
+            this.btnVotar.Name = "btnVotar";
+            this.btnVotar.Size = new System.Drawing.Size(75, 23);
+            this.btnVotar.TabIndex = 6;
+            this.btnVotar.Text = "Votar";
+            this.btnVotar.UseVisualStyleBackColor = true;
+            this.btnVotar.Click += new System.EventHandler(this.btnVotar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(276, 180);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(374, 180);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 8;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // frmVotacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVotar);
+            this.Controls.Add(this.btnFim);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblVoto);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVoto);
+            this.Name = "frmVotacao";
             this.Text = "frmVotacao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtVoto;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Label lblVoto;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnFim;
+        private System.Windows.Forms.Button btnVotar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnVoltar;
     }
 }
--- exercicio1/frmVotacao.cs
+++ exercicio1/frmVotacao.cs
@@ -16,5 +16,75 @@ namespace exercicio1
         {
             InitializeComponent();
         }
+        int cand1 = 0, cand2 = 0, cand3 = 0, final;
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var fm = new Form1();
+            fm.Show();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtFinal.Clear();
+            txtVoto.Clear();
+            txtVoto.Focus();
+        }
+
+        private void btnFim_Click(object sender, EventArgs e)
+        {
+            double vencedor;
+
+            if (cand1 > cand2 && cand1 > cand3)
+            {
+                vencedor = (cand1 * 100) / final;
+                txtFinal.Text = "José da Silva recebeu " + cand1 + " votos oque equivale a " + vencedor + "% dos votos.";
+            }
+            else
+            if (cand2 > cand1 && cand2 > cand3)
+            {
+                vencedor = (cand2 * 100) / final;
+                txtFinal.Text = "Maria Juruma recebeu" + cand2 + " votos oque equivale a " + vencedor + "% dos votos.";
+            }
+            else
+            if (cand3 > cand2 && cand3 > cand1)
+            {
+                vencedor = (cand3 * 100) / final;
+                txtFinal.Text = "João da Tapioca recebeu " + cand3 + " votos oque equivale a " + vencedor + "% dos votos.";
+            }
+            else
+            {
+                txtFinal.Text = "Empate";
+            }
+        }
+
+        private void btnVotar_Click(object sender, EventArgs e)
+        {
+            Double voto;
+
+            voto = Convert.ToDouble(txtVoto.Text);
+            if (voto == 1)
+            {
+                cand1++;
+                final++;
+                txtVoto.Clear();
+            }
+            else if (voto == 2)
+            {
+                cand2++;
+                final++;
+                txtVoto.Clear();
+            }
+            else if (voto == 3)
+            {
+                cand3++;
+                final++;
+                txtVoto.Clear();
+            }
+            else {
+                txtVoto.Clear();
+            }
+        }
     }
 }
